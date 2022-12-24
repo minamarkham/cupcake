1576783a59c24b72ce585af161066fcdcc2ec5ee
Mina Markham <mmarkham@slack-corp.com>
2022-12-23 23:55:52 -0800

    add collections

--- 11ty/collections.js
+++ 11ty/collections.js
@@ -3,4 +3,16 @@
  * https://www.11ty.dev/docs/collections/
  */
 
-module.exports = {};
+module.exports = {
+	pages: function (eleventyConfig) {
+		eleventyConfig.addCollection('pages', function (collectionApi) {
+			return collectionApi.getFilteredByGlob('src/content/pages/*.md');
+		});
+	},
+
+	posts: function (eleventyConfig) {
+		eleventyConfig.addCollection('posts', function (collectionApi) {
+			return collectionApi.getFilteredByGlob('src/content/posts/*.md');
+		});
+	},
+};
