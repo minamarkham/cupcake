3bc56b4f6c7f96da2a9cbadae124a05e73525712
Mina Markham <mmarkham@slack-corp.com>
2023-01-02 01:03:01 -0800

    add assets

--- 11ty/collections.js
+++ 11ty/collections.js
@@ -12,7 +12,21 @@ module.exports = {
 
 	posts: function (eleventyConfig) {
 		eleventyConfig.addCollection('posts', function (collectionApi) {
-			return collectionApi.getFilteredByGlob('src/content/posts/*.md');
+			return collectionApi.getFilteredByGlob('src/content/posts/**/*.md');
 		});
 	},
+
+	  projects: function (eleventyConfig) {
+		eleventyConfig.addCollection('projects', function (collectionApi) {
+			const projects = collectionApi.getFilteredByGlob('src/content/projects/*.njk');
+			return projects.sort((a, b) => Number(a.data.order) > Number(b.data.order) ? 1 : -1);
+		});
+	},
+
+	talks: function (eleventyConfig) {
+		eleventyConfig.addCollection('talks', function (collectionApi) {
+			return collectionApi.getFilteredByGlob('src/content/talks/*.md');
+		});
+	},
+
 };
