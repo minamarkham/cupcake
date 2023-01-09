c0603649c5abbef1836c5d362823c538f337fda4
Mina Markham <mmarkham@slack-corp.com>
2023-01-09 04:10:50 -0800

    update cms

---
+++ src/admin/index.js
@@ -0,0 +1,8 @@
+import CMS from 'netlify-cms-app'
+import Post from './templates/post.js';
+import Page from './templates/page.js';
+
+CMS.registerPreviewTemplate("posts", Post);
+CMS.registerPreviewTemplate("pages", Page);
+
+CMS.registerPreviewStyle("/assets/styles/main.css");
