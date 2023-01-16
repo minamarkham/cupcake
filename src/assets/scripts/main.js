f93a836258e7a5c34166c93ff9d7a9d90ff338ab
Mina Markham <mmarkham@slack-corp.com>
2023-01-16 05:39:39 -0800

    remove emojis from footer

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,10 +1,8 @@
 import copy from './modules/copy';
-import emojis from './modules/emojis';
 import { animateButton, handleSubmit } from './modules/form';
 import './modules/speedlify';
 
 copy();
-emojis();
 
 const button = document.getElementById("say-hi");
 if (button) button.addEventListener('click', animateButton, false);
